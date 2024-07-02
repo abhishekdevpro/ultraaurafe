598867a31f591a7e5f3640ed6cdc15a7cc75144c
Nandini <nandinigupta63696@gmail.com>
2024-07-02 11:20:33 +0530

    added toaster

--- src/components/sign-in/index.jsx
+++ src/components/sign-in/index.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Breadcrumb from "../bredcrumb/breadcrumb";
 import LoginForm from "../form/login-form";
+import { Toaster } from "react-hot-toast";
 
 const SignIn = () => {
   return (
