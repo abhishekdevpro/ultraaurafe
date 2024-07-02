d3b7a0caf711392cb25aedc4925cc032740d5544
Nandini <nandinigupta63696@gmail.com>
2024-07-02 11:36:27 +0530

    added axios

--- src/components/form/login-form.jsx
+++ src/components/form/login-form.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Link from 'next/link';
 import {toast} from 'react-hot-toast';
+import  axios  from "axios";
 // import { useRouter } from "next/router";
 // import { useNavigate } from "react-router";
 // import "./Login.css";
