7ac1748fb4bcdee01604fe054e4327a1603bd66e
Nandini <nandinigupta63696@gmail.com>
2024-07-18 16:29:14 +0530

    chatbot added

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,13 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          id="collect-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w, d) { w.CollectId = "659ff1c019aee0dd6428a0c5"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);`,
+          }}
+        />
       </body>
     </Html>
   )
